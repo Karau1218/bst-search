76a358f21f051f8295337481fd325b096898448c
Fkarau <30944677+Karau1218@users.noreply.github.com>
2026-03-04 21:08:27 -0800

    done w tests

--- src/BstSearch.java
+++ src/BstSearch.java
@@ -28,5 +28,6 @@ public class BstSearch {
 
     } else { 
         return contains(root.right, target);
+     }
     }
 }
--- src/BstSearchTest.java
+++ src/BstSearchTest.java
@@ -3,4 +3,56 @@ import org.junit.jupiter.api.Test;
 
 public class BstSearchTest {
 
+    private BinaryTreeNode<Integer> buildTree() {
+
+        BinaryTreeNode<Integer> root = new BinaryTreeNode<>(8);
+
+        root.left = new BinaryTreeNode<>(3);
+        root.right = new BinaryTreeNode<>(10);
+
+        root.left.left = new BinaryTreeNode<>(1);
+        root.left.right = new BinaryTreeNode<>(6);
+
+        root.right.right = new BinaryTreeNode<>(14);
+
+        return root;
+    }
+
+    @Test
+    public void testContainsLeftSide() {
+        BinaryTreeNode<Integer> root = buildTree();
+        assertTrue(BstSearch.contains(root, 6));
+    }
+
+    @Test
+    public void testContainsRightSide() {
+        BinaryTreeNode<Integer> root = buildTree();
+        assertTrue(BstSearch.contains(root, 14));
+    }
+
+    @Test
+    public void testContainsRoot() {
+        BinaryTreeNode<Integer> root = buildTree();
+        assertTrue(BstSearch.contains(root, 8));
+    }
+
+    @Test
+    public void testValueNotFound() {
+        BinaryTreeNode<Integer> root = buildTree();
+        assertFalse(BstSearch.contains(root, 7));
+    }
+
+    @Test
+    public void testEmptyTree() {
+        assertFalse(BstSearch.contains(null, 5));
+    }
+
+    @Test
+    public void testNullTarget() {
+        BinaryTreeNode<Integer> root = buildTree();
+
+        assertThrows(NullPointerException.class, () -> {
+            BstSearch.contains(root, null);
+        });
+    }
 }
